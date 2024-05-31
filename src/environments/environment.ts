3ded089a2876497229b19d5596e56e7767d65ec4
bossberrybaew <tanakun.wangpong@gmail.com>
2024-05-31 13:06:59 +0700

    init firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyAtjyliXqtjTG_zjVx29umbExyZAhic-RQ",
+        authDomain: "active-arena-5dbdb.firebaseapp.com",
+        databaseURL: "https://active-arena-5dbdb-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "active-arena-5dbdb",
+        storageBucket: "active-arena-5dbdb.appspot.com",
+        messagingSenderId: "753668954445",
+        appId: "1:753668954445:web:3aa4accf99bb72dce9bf04",
+        measurementId: "G-N5FZ2L1PKH"
+    }
+  };
